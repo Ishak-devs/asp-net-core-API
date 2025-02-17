463d82d85edd52f45c478c9c21d84c0783008fd5
ishakdév <kouicicontact@yahoo.com>
2025-02-17 20:18:49 +0100

    premier commit connexion reussie

---
+++ projet_aspnet_api/projet_aspnet_api/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace projet_aspnet_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public UsersController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Loginmodele loginUser)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Nom == loginUser.Nom && u.Password == loginUser.Password);
+            if (user == null)
+            {
+                return Unauthorized("Nom ou mot de passe invalide");
+            }
+            return Ok("Connexion réussie");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound("Utilisateur non trouvé");
+            }
+            return Ok(user);
+        }
+    }
+}
