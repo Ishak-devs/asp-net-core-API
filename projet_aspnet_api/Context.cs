608b057cf5d1ad309d982b9d8714f7dc1d1defdb
ishakdév <kouicicontact@yahoo.com>
2025-02-18 07:21:29 +0100

    ajout de EDIT

---
+++ projet_aspnet_api/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace projet_aspnet_api
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produits> Produits { get; set; }
+        public DbSet<Users> Users { get; set; }
+    }
+
+    public class Produits
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+    }
+
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Password { get; set; }
+    }
+}
