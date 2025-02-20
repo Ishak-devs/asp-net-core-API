6bdffa31a24531803008ac5216886d0d6c00d614
ishakdév <kouicicontact@yahoo.com>
2025-02-20 09:56:25 +0100

    autorisation au niveau de l'action

--- projet_aspnet_api/Controllers/RoleController.cs
+++ projet_aspnet_api/Controllers/RoleController.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 //using System.Linq;
 //using Microsoft.EntityFrameworkCore;
 using projet_aspnet_api;
+using System.Data;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -22,36 +23,37 @@ public class RoleController : ControllerBase
     public IActionResult Login(string username, string password)
     {
         var user = _context.Users.FirstOrDefault(u => u.Nom == username && u.Password == password);
-        if (user != null)
+        if (user == null)
         {
-            //Création d'un objet claims
-            var Myclaims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, username),
-                new Claim("Role", user.role)
-            };
-
-            //Identité claim regroupant l'ensemble des informations
-            var claimsIdentity = new ClaimsIdentity(Myclaims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-            //Création d'un cookie du shéma d'authentification
-            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-
-
-            //Retourne un message connexion réussie
-            return Ok(new { message = $"{user.role} logged in successfully", role = user.role });
-            
+            return Unauthorized(new { message = "Invalid credentials" });
         }
-        
-        //Retourne un message accès refusé
-        return Unauthorized("Invalid credentials");
+
+        // Création d'un objet claims
+        var Myclaims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, username),
+            new Claim("Role", user.role)
+        };
+
+        if (user.role != "admin")
+        {
+            return Unauthorized(new { message = "Vous n'êtes pas autorisé à accéder" });
+        }
+
+        // Identité claim regroupant l'ensemble des informations
+        var claimsIdentity = new ClaimsIdentity(Myclaims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        // Création d'un cookie du schéma d'authentification
+        HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+        // Retourne un message connexion réussie
+        return Ok(new { message = $"{user.role} logged in successfully", role = user.role });
     }
 
- 
     [HttpPost("logout")]
     public async Task<IActionResult> Logout()
     {
-        //Effacer le cookie
+        // Effacer le cookie
         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         return Ok("Logged out successfully");
     }
